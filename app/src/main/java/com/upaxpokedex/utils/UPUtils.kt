ac954a33a68b66acb02d683acaff4df855d54a0e
Alex Rodea <alexandra.rodea.dev@gmail.com>
2024-06-11 17:53:06 -0600

    [UPDATE] Delete unused imports

--- app/src/main/java/com/upaxpokedex/utils/UPUtils.kt
+++ app/src/main/java/com/upaxpokedex/utils/UPUtils.kt
@@ -1,12 +1,9 @@
 package com.upaxpokedex.utils
 
-import android.util.Log
 
 fun getNameInitials(name: String, lastname: String): String {
-    var initials = ""
-    if (name.isStartWithAlphabet() && name != "" && lastname != "" && lastname.isStartWithAlphabet()) {
-        initials = name.substring(0, 1) + lastname.substring(0, 1)
-    } else initials = name.substring(0, 1)
-    Log.e("Alex", "initials? = $initials")
+    val initials : String = if (name.isStartWithAlphabet() && name != "" && lastname != "" && lastname.isStartWithAlphabet()) {
+        name.substring(0, 1) + lastname.substring(0, 1)
+    } else name.substring(0, 1)
     return initials.uppercase()
 }
