a536adc602f2e3ce0b8e38e9e3992853109b42a7
AlexRGaon <arodea@gaon.mx>
2024-06-11 10:59:16 -0600

    [TEST]

--- app/src/main/java/com/upaxpokedex/MainActivity.kt
+++ app/src/main/java/com/upaxpokedex/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    //This is a test
                     Greeting("Android")
                 }
             }
